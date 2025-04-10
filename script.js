f235bf2fd56029c8e00ef64fec6d11ca6e639c40
DivinePC <dcperalta812@gmail.com>
2025-04-10 17:27:17 -0400

    Log object properties and methods.

--- script.js
+++ script.js
@@ -11,3 +11,7 @@ const student = {
         console.log('It is ' + this.enrolled + ' that ' + this.name + ' is enrolled in these courses: ' + this.courses);
     }
 };
+
+console.log(student.name + ' is ' + student.age + ' years old.'); // Log name and age properties
+student.info(); // Call info method
+student.enrolment(); // Call enrolment method
